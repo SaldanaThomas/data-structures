8ccaa1a17c9bdc2638cdc1f178913c208abe28f7
Thomas Saldana <saldana.tom@gmail.com>
2022-10-26 16:22:59 -0700

    Completed prototypal queue implementation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.size() + 1] = value;
+  },
+
+  dequeue: function() {
+    var oldestItem = this.storage[1];
+    for (var key in this.storage) {
+      if (key < this.size()) {
+        this.storage[key] = this.storage[Number(key) + 1];
+      } else {
+        delete this.storage[key];
+      }
+    }
+    return oldestItem;
+  },
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
