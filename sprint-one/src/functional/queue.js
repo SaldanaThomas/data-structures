f1724a039ca0068ba17235a30d341f858a2e6eb7
Thomas Saldana <saldana.tom@gmail.com>
2022-10-26 16:21:31 -0700

    Completed functional queue implementation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size() + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var oldestValue = storage[1];
+    for (var key in storage) {
+      if (key < someInstance.size()) {
+        storage[key] = storage[Number(key) + 1];
+      } else {
+        delete storage[key];
+      }
+    }
+    return oldestValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
